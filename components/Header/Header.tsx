f2897a8758e7a73413d8584b74fc1f4f27ff59ed
nstanev255 <nikolay.stanev.255@gmail.com>
2023-09-22 13:09:10 +0300

    added more eslint rules

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,21 @@
-import { Container } from '@mui/material';
+import { Grid } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Header() {
-  return <Container></Container>;
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <Image src="/favicon.png" width={50} height={50} alt="FreeBird"></Image>
+      </Grid>
+      <Grid item>
+        <Stack direction="row" spacing={2}>
+          <Link href="/">Home</Link>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Header from '@/components/Header/Header';
 
 export default function Home() {
   return (
@@ -10,8 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
-
+        <Header />
       </main>
     </>
-  )
+  );
 }
